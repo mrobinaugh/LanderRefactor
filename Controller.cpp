2262b3161de24b21aa537c0fead5d5b27c372bf2
Matthew Robinaugh <mrobinaugh@pop-os.localdomain>
2023-08-08 07:56:26 -0600

    Modify return type to double

--- Controller.cpp
+++ Controller.cpp
@@ -3,13 +3,13 @@
 #include "Controller.h"
 
 
-static double Controller::getCommandedMoment(double intertia, double theta, double omega){
+double Controller::getCommandedMoment(double inertia, double theta, double omega){
     double kp = 1;
     double kd = 1;
     double thetaDesired = 0.0;
     double thetaCommand = thetaDesired - theta;
     double omegaCommand = thetaCommand * kp - omega;
     double alphaDesired = omegaCommand * kd;
-    double momentCommand = alphaDesired * Inertia;
+    double momentCommand = alphaDesired * inertia;
     return momentCommand;
 }
